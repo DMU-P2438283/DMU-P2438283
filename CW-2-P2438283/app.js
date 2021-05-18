0436c4032e846543e983bd6bb33833201107c06c
Stojan Pejic <72386137+DMU-P2438283@users.noreply.github.com>
2021-05-18 10:49:43 +0100

    Add files via upload

--- CW-2-P2438283/app.js
+++ CW-2-P2438283/app.js
@@ -1,14 +1,21 @@
+//the query selector is meant to select the property which is burger, this property is selected from the html page and burger is the property of the 3 lines
+
 const navSlide = () => {
   const burger = document.querySelector('.burger');
   const nav = document.querySelector('.nav-links');
   const navLinks = document.querySelectorAll('.nav-links li');
-    //Toggle Nav
+
+    //Toggle Nav, the event listener is ready when activated on click from the toggle
+
   burger.addEventListener('click' , () => {
     nav.classList.toggle('nav-active');
 
-    
 
-    //Animate Links
+
+    //Animate Links, these links are set to fade so that when the bar comes out, the links fade in after 0.5 seconds
+
+    //this is done style animation which when set to fade, will create a fading affect
+
     navLinks.forEach((link, index) => {
       if (link.style.animation) {
         link.style.animation = '';
@@ -25,3 +32,49 @@ const navSlide = () => {
 }
 
 navSlide();
+
+window.addEventListener("DOMContentLoaded", function () {
+  // get the form elements defined in your form HTML above
+
+  var form = document.getElementById("my-form");
+  // var button = document.getElementById("my-form-button");
+  var status = document.getElementById("status");
+
+  // Success and Error functions for after the form is submitted
+
+  function success() {
+    form.reset();
+    status.classList.add("success");
+    status.innerHTML = "Thanks!";
+  }
+
+  function error() {
+    status.classList.add("error");
+    status.innerHTML = "Oops! There was a problem.";
+  }
+
+  // handle the form submission event
+
+  form.addEventListener("submit", function (ev) {
+    ev.preventDefault();
+    var data = new FormData(form);
+    ajax(form.method, form.action, data, success, error);
+  });
+});
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
+}
